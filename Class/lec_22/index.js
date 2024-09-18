ccf31415fbd7fe664648002be3a7719826535593
ManveerBains1000 <manveerjaat95@gmail.com>
2024-09-18 14:17:50 +0530

    add chess game

---
+++ Class/lec_22/index.js
@@ -0,0 +1,44 @@
+// console.log("Manveer");
+// var a = 10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+// a= 30;
+// console.log(a);
+// let  b = 10;
+// console.log(b);
+// b = 20;
+// console.log(b);
+// const: reduecloaration not possible and assignment is also not possible
+// let: redeclaration not possible and assignement is possible
+// var: redeclaration is possible and assignement is also possible
+
+// Number
+let a = 10;
+console.log(typeof(a));
+let b = 10.2;
+console.log(typeof(b));
+
+//String
+let Name = "Manveer";
+console.log(typeof(Name));
+console.log(Name);
+
+//Boolean
+let check = true;
+console.log(typeof(check));
+
+//undefined
+let c ;
+console.log(typeof(c));
+
+c = 20;
+console.log(typeof(c));
+
+//null
+let d = null;
+console.log(typeof(d));
+
+let e = 5;
+e += 2;
+console.log(e);
---
+++ Class/lec_24/iterator.js
@@ -0,0 +1,15 @@
+// let str = "Manveer";
+// let obj = {
+//     name:"Manveer"
+// };
+// for (let i in obj){
+//     console.log(i);
+// }
+function sum(a,b){
+    return a+b;
+}
+function mul(a,b){
+    return a*b;
+}
+let a= mul(5,5);
+console.log(a);
